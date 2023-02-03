15ea158a75d3e5e7cd8571822b678f52981c82c5
officialvencee <officialvencee.manansala@gmail.com>
2023-02-03 23:46:31 +0800

    Adding BackArrowButton function in order to reuse in other screen

--- app/src/main/java/com/lbo/mobile/laybare/shared/components/BackButton.kt
+++ app/src/main/java/com/lbo/mobile/laybare/shared/components/BackButton.kt
@@ -1,5 +1,9 @@
 package com.lbo.mobile.laybare.shared.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
@@ -8,6 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.lbo.mobile.laybare.shared.util.LOGIN_BUTTONS_COLOR
 
 @Preview
 @Composable
@@ -18,6 +25,19 @@ fun BackButtonComponent(modifier:Modifier = Modifier,iconTint:Long = 0xFFFFFFFF,
             contentDescription = "Arrow Back",
             tint = Color(iconTint)
         )
-        
+
+    }
+}
+
+@Composable
+fun BackArrowButton(navController: NavController){
+    Column(modifier = Modifier.padding(top = 40.dp)) {
+        BackButtonComponent(modifier = Modifier
+            .height(25.dp)
+            .width(30.dp)
+            .padding(start = 5.dp), iconTint = LOGIN_BUTTONS_COLOR
+        ){
+            navController.popBackStack()
+        }
     }
 }
