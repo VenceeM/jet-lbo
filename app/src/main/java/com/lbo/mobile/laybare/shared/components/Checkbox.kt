806e1630a139b11f544dcf9ce380f8cd50100cf3
officialvencee <officialvencee.manansala@gmail.com>
2023-02-03 23:47:17 +0800

    Creating a CheckboxComponent to reuse in other screen

---
+++ app/src/main/java/com/lbo/mobile/laybare/shared/components/Checkbox.kt
@@ -0,0 +1,17 @@
+package com.lbo.mobile.laybare.shared.components
+
+import androidx.compose.material.Checkbox
+import androidx.compose.material.CheckboxDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CheckboxComponent(
+    modifier: Modifier = Modifier,isChecked:Boolean = false,onCheckedChange:(Boolean)->Unit = {},
+    ){
+
+    Checkbox(modifier = modifier,checked = isChecked, onCheckedChange = {onCheckedChange(it)},
+    colors = CheckboxDefaults.colors())
+}
