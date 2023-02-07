085e04197933add9d556a57e38f14d6b29ff2f83
officialvencee <officialvencee.manansala@gmail.com>
2023-02-07 21:17:44 +0800

    Add password and emailOrPhone variable

--- app/src/main/java/com/lbo/mobile/laybare/main/presentation/screen/login/LoginState.kt
+++ app/src/main/java/com/lbo/mobile/laybare/main/presentation/screen/login/LoginState.kt
@@ -4,7 +4,9 @@ import com.lbo.mobile.laybare.main.domain.model.login.LoginResponse
 import retrofit2.Response
 
 data class LoginState(
-    var loading:Boolean? = false,
+    var loading:Boolean = false,
     var login:LoginResponse? = null,
-    var message:String? = ""
+    var message:String? = "",
+    var password:String = "",
+    var emailOrPhone:String = ""
 )
